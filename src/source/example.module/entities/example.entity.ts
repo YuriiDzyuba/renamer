f35f9df8ff755b6edfe6af01d4510760d72161b8
YuriiDzyuba <iurii11022@gmail.com>
2022-06-10 12:00:40 +0300

    Add apiProperty decorators to entity

--- src/source/example.module/entities/example.entity.ts
+++ src/source/example.module/entities/example.entity.ts
@@ -1,4 +1,5 @@
 import { Example } from '../types/example.type';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -9,15 +10,19 @@ import {
 
 @Entity({ name: 'examples' })
 export class ExampleEntity implements Example {
+  @ApiProperty({ example: '99f29076-b481-4ae6-916d-6cbc4c2fc2a9' })
   @PrimaryGeneratedColumn('uuid')
   exampleId: string;
 
+  @ApiProperty({ description: 'some example', example: 'Example-Bank' })
   @Column()
   exampleName: string;
 
+  @ApiProperty({ description: 'timestamp when entity has been created', example: new Date() })
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty({ description: 'timestamp when entity has been updated', example: new Date() })
   @UpdateDateColumn()
   updatedAt: Date;
 }
